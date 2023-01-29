f314aa4a441f1aa94915c1b0b24f40df2657af87
yinhang_sun <sunyinhang@gmail.com>
2023-01-29 08:24:49 -0800

    Understanding Problems with Stubs

--- src/main/java/com/yinhang/mockito/mockitodemo/business/SomeBusinessImpl.java
+++ src/main/java/com/yinhang/mockito/mockitodemo/business/SomeBusinessImpl.java
@@ -1,14 +1,19 @@
 package com.yinhang.mockito.mockitodemo.business;
 
 public class SomeBusinessImpl {
-	
+
 	private DataService dataService;
-	
+
+	public SomeBusinessImpl(DataService dataService) {
+		super();
+		this.dataService = dataService;
+	}
+
 	public int findTheGreatestFromAllData() {
 		int[] data = dataService.retrieveAllData();
 		int greatestValue = Integer.MIN_VALUE;
-		for(int value : data) {
-			if(value > greatestValue) {
+		for (int value : data) {
+			if (value > greatestValue) {
 				greatestValue = value;
 			}
 		}
